87456649180514a771e29d026ebcc80a402eb9e1
Pushpansh Upadhayay <pushpansh.upadhayay@gmail.com>
2022-05-26 02:08:45 +0530

    updated according to the employee_data.csv

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import cust
 import json
 import phe as paillier
 import servercalc
-
-if __name__=='__main':
-    cust.storeKeys()
-    pub_key, priv_key = cust.getKeys()
-    data = age, he, al, gen = [24,4,6,1]
+import pandas as pd
+cust.storeKeys()
+pub_key, priv_key = cust.getKeys()
+df = pd.read_csv('employee_data.csv')
+l = []
+for i in df.head().index:
+    a = list(df.iloc[i, 0:4])
+    l.append(a)
+for i in l:
+    data = i
     datafile = cust.serializeData(pub_key, data)
     with open('data.json', 'w') as file:
         json.dump(datafile,file)
-
+    servercalc.writeToAnswer()
     answer_file = cust.loadAnswer()
     answer_key = paillier.PaillierPublicKey(n=int(answer_file['pubkey']['n']))
     answer = paillier.EncryptedNumber(answer_key,int(answer_file['values'][0]), int(answer_file['values'][1]))
